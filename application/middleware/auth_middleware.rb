729cc6a9b99ac43d87e77ca379bc3ac5d09dbf7c
Vladimir Kuzmich <vjk1976@ukr.net>
2019-09-19 17:22:45 +0300

    refactoring middleware

--- application/middleware/auth_middleware.rb
+++ application/middleware/auth_middleware.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AuthMiddleware
+  GAME_LINKS = ['/check', '/show_hint', '/win', '/lose'].freeze
+
   def initialize(app, status = 302)
     @app = app
     @status = status
@@ -9,12 +11,16 @@ class AuthMiddleware
   def call(env)
     @request = Rack::Request.new(env)
 
-    return [@status, { 'Location' => GameProcess::URLS[:game] }, ['']] if authenticated?
+    return [@status, { 'Location' => GameProcess::URLS[:game] }, ['']] if authenticated? && game_links?
 
     @app.call(env)
   end
 
   def authenticated?
-    @request.session.key?(:root) && @request.session.key?('username')
+    @request.session.key?(:game) && @request.session.key?('username')
+  end
+
+  def game_links?
+    GAME_LINKS.include?(@request.path)
   end
 end
--- config.ru
+++ config.ru
@@ -2,8 +2,8 @@
 
 require_relative 'autoload'
 
-use AuthMiddleware
 use Rack::Reloader, 0
-use Rack::Session::Cookie, key: 'rack.session', path: '/', expire_after: 604_800, secret: 'snowden'
+use Rack::Session::Cookie, key: 'rack.session', path: '/', expire_after: 604_800, secret: 'wi fi'
+use AuthMiddleware
 
 run Rack::Cascade.new([Rack::File.new('views'), GameProcess])
